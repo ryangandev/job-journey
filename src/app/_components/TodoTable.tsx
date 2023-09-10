7df0a90d5db780b1beb643a1b1fbe7930ade631d
Ryan Gan <49723210+ryanzg123@users.noreply.github.com>
2023-09-10 12:49:54 -0400

    update project structure

---
+++ src/app/_components/TodoTable.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import { prisma } from "@/app/_libs/db"
+
+function TodoTable() {
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            const response = await prisma.todo.findMany()
+            console.log("Todo: ", response)
+        }
+
+        fetchTodos()
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TodoTable;
