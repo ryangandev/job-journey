4bb79b0cbb5dc0a7518ec9184afa58ae97dea2a2
Ryan Gan <49723210+ryanzg123@users.noreply.github.com>
2023-09-10 13:38:38 -0400

    move all server actions functions to a designated space

--- app/_actions.ts
+++ app/_actions.ts
@@ -1,6 +1,7 @@
 'use server'
 
-import { prisma } from "./_libs/db"
+import { prisma } from "@/_libs/db"
+import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
 
 export async function createTodoAction(data: FormData) {
@@ -8,5 +9,14 @@ export async function createTodoAction(data: FormData) {
     if (typeof title !== "string" || title.length === 0) return
 
     await prisma.todo.create({ data: { title, isCompleted: false}})
-    redirect("/")
+    revalidatePath('/')
+}
+
+export async function toggleTodoAction(id: string, isCompleted: boolean) {
+    await prisma.todo.update({ where: { id }, data: { isCompleted }})
+}
+
+export async function deleteTodoAction(id: string) {
+    await prisma.todo.delete({ where: { id }})
+    revalidatePath('/')
 }
