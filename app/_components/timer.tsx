ec32c809e830372e246a00a18e50a44084dca5da
Ryan Gan <49723210+ryanzg123@users.noreply.github.com>
2023-09-15 14:53:38 -0400

    added timer component

---
+++ app/_components/timer.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+function Timer() {
+    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentDateTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <span>
+            {currentDateTime.toLocaleString("en-US", { hour12: false })}
+        </span>
+    );
+}
+
+export default Timer;
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,21 @@ import CreateTodoForm from "@/_components/create-todo-form";
 import TodoTable from "@/_components/todo-table";
 import { getTodosAction } from "@/_actions";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import Timer from "./_components/timer";
 
 export default async function Home() {
     const todos = await getTodosAction();
 
     return (
-        <Card className="w-full lg:w-[1000px] h-[600px]">
-            <CardHeader className="flex gap-4 items-center">
-                <h1 className="text-2xl font-extrabold">Todo Dashboard</h1>
-                <CreateTodoForm />
+        <Card className="w-full lg:w-[1000px] h-[600px] outline-dashed">
+            <CardHeader className="flex items-center justify-between">
+                <div className="flex gap-4">
+                    <h1 className="text-2xl font-extrabold">Todo Dashboard</h1>
+                    <CreateTodoForm />
+                </div>
+                <h1 className="text-2xl font-extrabold">
+                    <Timer />
+                </h1>
             </CardHeader>
             <CardBody className="">
                 <TodoTable todos={todos} />
