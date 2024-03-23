39cb8bb74d0293db6053645f47c073e079a5003a
Ryan Gan <ganzhiheng1@gmail.com>
2024-03-23 12:48:44 -0400

    update tailwind config in compatible with src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     content: [
         "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {},
