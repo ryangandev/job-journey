97307243de16ea9184c85ee73c574b70348c4615
Ryan Gan <ganzhiheng1@gmail.com>
2024-03-24 17:39:19 -0400

    Installed zod and created schema for application detail

---
+++ src/constants/schema.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+const QuestionAndAnswerSchema = z.object({
+    question: z.string(),
+    answer: z.string(),
+});
+
+const ApplicationDetailSchema = z.object({
+    id: z.string(),
+    company: z.string(),
+    title: z.string(),
+    location: z.string(),
+    remote: z.enum(["on_site", "remote"]),
+    status: z.enum([
+        "applied",
+        "interviewing",
+        "offered",
+        "rejected",
+        "ghosted",
+    ]),
+    isFavorite: z.boolean(),
+    replied: z.boolean(),
+    interviewAquired: z.boolean(),
+    appliedAt: z.date(),
+    updatedAt: z.date(),
+    link: z.string(),
+    applicationQA: z.array(QuestionAndAnswerSchema),
+    updates: z.array(z.string()),
+});
+
+export { ApplicationDetailSchema, QuestionAndAnswerSchema };
