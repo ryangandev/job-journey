2fc9d2b7fcf418301a47091fa17cf100c455dc1b
Ryan Gan <ganzhiheng1@gmail.com>
2024-03-30 21:27:50 -0400

    adding custom font colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ module.exports = {
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                dark: {
+                    100: "#ffffff",
+                    200: "#e3e4e6",
+                    300: "#a1a1a1",
+                    400: "#959699",
+                },
+                light: {
+                    100: "#1b1b1b",
+                    200: "#2f2f31",
+                    300: "#666666",
+                    400: "#5c5c5e",
+                },
+            },
+        },
     },
     darkMode: "class",
     plugins: [nextui()],
