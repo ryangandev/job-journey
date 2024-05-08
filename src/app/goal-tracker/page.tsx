dedacea7c01b132c3ed0047e4d8ce46ce3db71d6
Ryan Gan <ganzhiheng1@gmail.com>
2024-05-07 22:13:00 -0400

    Set up initial routes for new features

---
+++ src/app/goal-tracker/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GoalTracker() {
+    return <div>GoalTracker</div>;
+}
---
+++ src/app/interview-prep/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function InterviewPrep() {
+    return <div>InterviewPrep</div>;
+}
