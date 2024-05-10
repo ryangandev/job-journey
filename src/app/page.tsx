f1ee59236a85eaf218dda53231b2d184c5797fe4
Ryan Gan <ganzhiheng1@gmail.com>
2024-05-10 15:47:43 -0400

    chore: update homepage width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomepageOptions from "../components/homepage/homepage-options";
 
 export default function Home() {
     return (
-        <main className="w-screen py-4 px-20 lg:px-40 flex flex-col justify-center items-center pt-24 sm:pt-36 space-y-16">
+        <main className="max-w-7xl mx-auto py-4 px-20 lg:px-40 flex flex-col justify-center items-center pt-24 sm:pt-36 space-y-16">
             <h1 className="font-bold text-5xl text-[#171717]/90 dark:text-[#ededed]/90 text-center leading-normal">
                 The One Management Tool You Need To Streamline Your Job Process
             </h1>
