65841df8b0d15786c05c3f1dffa97219a94b6a79
Ryan Gan <ganzhiheng1@gmail.com>
2024-05-17 22:11:54 -0400

    Extend session type manually to adding new role field to session object

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,38 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 
 import { prisma } from "./libs/db";
+import { getUserById } from "./data/user";
 import authConfig from "./auth.config";
 
+// To Assign new fields to the session object, we need to use the callbacks object
+// Assign the new field to the token object in the jwt callback first
+// Then assign the new field to the session object in the session callback
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+
+            return session;
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+            return token;
+        },
+    },
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" }, // Use JWT here because we cannot use database sessions with Prisma, it does not work on the Edge
     ...authConfig,
