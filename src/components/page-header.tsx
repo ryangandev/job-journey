e0ac0b944192e0202c2f651d2240ea7610bc5ff1
ryangandev <49723210+ryangandev@users.noreply.github.com>
2024-09-23 14:49:57 -0400

    rework from the ground up

--- src/components/page-header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-type PageHeaderProps = {
-  children: React.ReactNode;
-};
-export default function PageHeader({ children }: PageHeaderProps) {
-  return <h2 className="text-3xl font-semibold capitalize">{children}</h2>;
-}
