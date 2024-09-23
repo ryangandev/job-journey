d40919710065e1bcaddd6feff2287a1cb73508ee
ryangandev <49723210+ryangandev@users.noreply.github.com>
2024-09-23 15:47:27 -0400

    Setup

---
+++ src/providers/app-providers.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { NextUIProvider } from '@nextui-org/react'
+
+import { ThemeProvider } from '@/providers/theme-provider';
+
+export default function AppProviders({ children }: { children: React.ReactNode }) {
+    const router = useRouter();
+
+    return (
+        <NextUIProvider navigate={router.push}>
+            <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        // disableTransitionOnChange
+      >
+
+{children}
+
+      </ThemeProvider>
+        </NextUIProvider>
+    )
+}
