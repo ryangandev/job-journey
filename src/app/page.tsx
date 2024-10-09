1483ac006aad2864bcc0ed3f27efed6fabe26fe7
ryangandev <49723210+ryangandev@users.noreply.github.com>
2024-10-08 23:27:56 -0400

    responsive hero title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center space-y-20 px-4 py-6">
       <Link href="/login">Sign In</Link>
+      <h1 className="hero max-w-5xl text-center text-5xl font-bold">
+        The One Management Tool You Need To Streamline Your Job Process
+      </h1>
     </main>
   );
 }
